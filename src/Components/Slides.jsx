7ee6713ec5e2d1d44bef024577c0835089c36ce1
Renato Santos <renatosantosdeoliveirac@gmail.com>
2023-11-14 19:06:01 -0300

    Retirada dos impórts do react, axios e do componente Modal, retirada da useStates open, videoURL e URL, retirada da função handleOpen, no return do map tirado o button e inserido o Link junto com um button

--- src/Components/Slides.jsx
+++ src/Components/Slides.jsx
@@ -1,9 +1,8 @@
 import { Box, IconButton, Button } from '@mui/material'
+import { Link } from 'react-router-dom';
 import { KeyboardArrowLeft, KeyboardArrowRight } from '@mui/icons-material';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow'
-import { useRef, useState } from 'react'
-import axios from 'axios'
-import Modal from './Modal'
+import { useRef } from 'react'
 import './Styles/Slides.css'
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
@@ -12,9 +11,6 @@ import '@fontsource/roboto/700.css'
 
 export default function Slide_Movies(props){
 
-    const [open, setOpen] = useState(false)
-    const [videoURL, setVideoURL] = useState(null)
-    const [URL, setURL] = useState(null)
     const imageURL = 'https://image.tmdb.org/t/p/original';
     const carousel = useRef(null)
     const width = window.innerWidth + 'px'
@@ -25,28 +21,6 @@ export default function Slide_Movies(props){
     const handleRight = () =>{
         carousel.current.scrollLeft += carousel.current.offsetWidth
     }
-    const handleOpen = id =>{
-        console.log(id)
-        const video = {
-            method: 'GET',
-            url: `https://api.themoviedb.org/3/movie/${id}/videos?language=pt-BR&page=1`,
-            headers: {
-              accept: 'application/json',
-              Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNGNhMDkwOTYyYjlkY2YxZjYyNzhjNjQ3YWI1YzhmNSIsInN1YiI6IjY1MzdlZmUxNDFhYWM0MDBhYTA4MTIzOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.T0YHaxg5E2HUn_wnrvxue_wwmqslufrrwZOJ10jgcjo'
-            }
-          };
-
-        axios
-        .request(video)
-        .then(function (response) {
-          setVideoURL(response.data)
-          setOpen(true)
-          setURL(videoURL.results[0].key)
-        })
-        .catch(function (error) {
-          console.error(error);
-        });
-      }
 
     return(
         <Box className='box'
@@ -55,9 +29,6 @@ export default function Slide_Movies(props){
                 display: 'flex',
                 flexDirection: 'column'
             }}>
-               {videoURL ? 
-                <Modal open={open} setOpen={setOpen} id={URL} /> 
-                : ''}
                 <span>{props.title}</span>
                 <ul ref={carousel}>
             {props.movies ?
@@ -73,9 +44,11 @@ export default function Slide_Movies(props){
                             }}
                             >
                                 <div className='play'>
-                                    <Button className='play_movie' variant='contained' size='small' onClick={handleOpen(item.id)}>
-                                        <PlayArrowIcon />
-                                    </Button>
+                                    <Link to={'/Assistir/' + item.id} className='link'>
+                                        <Button className='play_movie' variant='contained' size='small' >
+                                            <PlayArrowIcon />
+                                        </Button>
+                                    </Link>
                                 </div>
                             </Button>
                         </li>
