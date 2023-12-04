69336a5869c91ace83686698caf69ad8c8b8f524
Renato Santos <renatosantosdeoliveirac@gmail.com>
2023-12-04 20:39:45 -0300

    Criando o footer do App

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,86 @@
+import { Box, Grid, Button, IconButton, Link } from '@mui/material';
+import { NavLink } from "react-router-dom";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import './Styles/Footer.css'
+
+
+export default function Footer(){
+    return(
+        <Box 
+        sx={{
+            width: '100%',
+            margin: '1% 0',
+            backgroundColor: 'transparent',
+            color: '#fff'
+        }}>
+            <Grid container xl={12} lg={12} md={12} xs={12} alignItems={'center'} className='grid_container_footer'>
+                <Grid item xl={4} lg={4} md={4} xs={4} className='grid_logo'
+                sx={{textAlign: 'center'}}>
+
+                    <Button 
+                    sx={{color: 'red',
+                         fontSize: '1.3rem'}}>
+                        MOVIEFLIX
+                    </Button>
+
+                </Grid>
+                <Grid item container xl={4} lg={4} md={4} xs={4} className='grid_links'
+                    justifyContent={'center'} 
+                    alignItems={'center'} 
+                    direction={'column'}>
+
+                    <Grid item>
+                        <span>Páginas</span>
+                    </Grid>
+
+                    <Grid item className='item_link'>
+                        
+                        <div className='link_active'>
+                            <NavLink to='/' className='grid_button_link'>
+                                <Button variant='text'>Início</Button>
+                            </NavLink>
+
+                            <NavLink to='/Séries' className='grid_button_link'>
+                                <Button variant='text'>Séries</Button>
+                            </NavLink>
+
+                            <NavLink to='/Filmes' className='grid_button_link'>
+                                <Button variant='text'>Filmes</Button>
+                            </NavLink>
+
+                            <NavLink to='/Bombando' className='grid_button_link'>
+                                <Button variant='text'>Bombando</Button>
+                            </NavLink>
+                        </div>
+                            
+                    </Grid>
+
+                </Grid>
+                <Grid item container xl={4} lg={4} md={4} xs={4} className='grid_social'
+                    justifyContent={'space-evenly'} 
+                    alignItems={'center'} 
+                    direction={'column'}>
+
+                    <Grid item className='grid_span'>
+                        <span>Social e Links</span>
+                    </Grid>
+
+                    <Grid item className='grid_content'>
+                            <Link href='https://github.com/renatosantosc' underline='none' target='_blank' rel='noopener'>
+                                <IconButton>
+                                    <GitHubIcon sx={{color: '#d1d1d1', fontSize: '1.2rem'}} />
+                                </IconButton>
+                            </Link>
+
+                        <Button variant='text'>
+                            <Link href='https://www.themoviedb.org/' underline='none' target='_blank' rel='noopener' sx={{color: '#d1d1d1'}}>
+                                TMDB API
+                            </Link>
+                        </Button>
+                    </Grid>
+
+                </Grid>
+            </Grid>
+        </Box>
+    )
+}
